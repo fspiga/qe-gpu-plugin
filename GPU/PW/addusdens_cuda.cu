edf67ba0f5fcc654d0798420045c386d2d7f66db
Filippo Spiga <spiga.filippo@gmail.com>
2012-11-26 00:22:20 +0000

    CUDA kernel kernel_compute_aux rewritte. It might be further optimized. It seems to work on GPU with CC 5.5

--- GPU/PW/addusdens_cuda.cu
+++ GPU/PW/addusdens_cuda.cu
@@ -21,50 +21,45 @@ extern "C" void stop_clock_(char * label, unsigned int length_arg );
 __global__ void kernel_compute_aux( const double * __restrict eigts1, const double * __restrict eigts2, const double * __restrict eigts3,
 		const int * __restrict ig1, const int * __restrict ig2, const int * __restrict ig3,  const int nr1, const int nr2, const int nr3,
 		const double * __restrict qgm, const double * __restrict becsum, double * aux, const int na, const int nspin_mag, const int ngm,
-		const int first_becsum, const int ijh, const int nat, const int * __restrict ityp, const int nt )
+		const int first_becsum, const int ijh, const int nat, const int it )
 {
-	register int is = blockIdx.x * blockDim.x + threadIdx.x;
-	register int ig = blockIdx.y * blockDim.y + threadIdx.y;
-	register int threads_id = threadIdx.y * blockDim.x + threadIdx.x;
+	register int ig = blockDim.x * blockIdx.x + threadIdx.x;
+	// register int igg = ig * 2;
+	register int is;
+	register int s_ig1, s_ig2, s_ig3;
 	
 	double eigts1_local[2], eigts2_local[2], eigts3_local[2], skk[2], element[2], sup, s_qgm[2], aux_sup[2];
-	int s_ig1, s_ig2, s_ig3;
+
+	if( ig < ngm ) {
 	
-	__shared__ int s_ityp[1024];
-	
-	if( threads_id < nat )
-		s_ityp[ threads_id ] = ityp[threads_id];
-	
-	if( ig < ngm && is < nspin_mag ) {
-	
-		s_qgm[ 0 ] = qgm[ ig * 2 ];
-		s_qgm[ 1 ] = qgm[ ( ig * 2 ) + 1];
-		aux_sup[0] = aux[ ( ig + (is * ngm) ) * 2 ];
-		aux_sup[1] = aux[ ( ( ig + (is * ngm) ) * 2 ) + 1 ];
 		s_ig1 = ig1[ig];
 		s_ig2 = ig2[ig];
 		s_ig3 = ig3[ig];
 
-		for ( int ina = 0; ina < nat; ina++) {
+		s_qgm[ 0 ] = qgm[ ig * 2 ];
+		s_qgm[ 1 ] = qgm[ ( ig * 2 ) + 1];
 
-			if ( s_ityp[ina] == nt ) {
+		eigts1_local[0] = eigts1[ ( ( (nr1 + s_ig1) + ( it * ( nr1 * 2 + 1 ) ) ) * 2 ) ];
+		eigts1_local[1] = eigts1[ ( ( (nr1 + s_ig1) + ( it * ( nr1 * 2 + 1 ) ) ) * 2 ) + 1 ];
 
-			eigts1_local[0] = eigts1[ ( ( (nr1 + s_ig1) + ( ina * ( nr1 * 2 + 1 ) ) ) * 2 ) ];
-			eigts1_local[1] = eigts1[ ( ( (nr1 + s_ig1) + ( ina * ( nr1 * 2 + 1 ) ) ) * 2 ) + 1 ];
+		eigts2_local[0] = eigts2[ ( ( (nr2 + s_ig2) + ( it * ( nr2 * 2 + 1 ) ) ) * 2 ) ];
+		eigts2_local[1] = eigts2[ ( ( (nr2 + s_ig2) + ( it * ( nr2 * 2 + 1 ) ) ) * 2 ) + 1 ];
 
-			eigts2_local[0] = eigts2[ ( ( (nr2 + s_ig2) + ( ina * ( nr2 * 2 + 1 ) ) ) * 2 ) ];
-			eigts2_local[1] = eigts2[ ( ( (nr2 + s_ig2) + ( ina * ( nr2 * 2 + 1 ) ) ) * 2 ) + 1 ];
+		eigts3_local[0] = eigts3[ ( ( (nr3 + s_ig3) + ( it * ( nr3 * 2 + 1 ) ) ) * 2 ) ];
+		eigts3_local[1] = eigts3[ ( ( (nr3 + s_ig3) + ( it * ( nr3 * 2 + 1 ) ) ) * 2 ) + 1 ];
 
-			element[0] = eigts1_local[0] * eigts2_local[0] - eigts1_local[1] * eigts2_local[1];
-			element[1] = eigts1_local[0] * eigts2_local[1] + eigts1_local[1] * eigts2_local[0];
+		element[0] = eigts1_local[0] * eigts2_local[0] - eigts1_local[1] * eigts2_local[1];
+		element[1] = eigts1_local[0] * eigts2_local[1] + eigts1_local[1] * eigts2_local[0];
 
-			eigts3_local[0] = eigts3[ ( ( (nr3 + s_ig3) + ( ina * ( nr3 * 2 + 1 ) ) ) * 2 ) ];
-			eigts3_local[1] = eigts3[ ( ( (nr3 + s_ig3) + ( ina * ( nr3 * 2 + 1 ) ) ) * 2 ) + 1 ];
+		skk[0] = element[0] * eigts3_local[0] - element[1] * eigts3_local[1];
+		skk[1] = element[1] * eigts3_local[0] + element[0] * eigts3_local[1];
 
-			skk[0] = element[0] * eigts3_local[0] - element[1] * eigts3_local[1];
-			skk[1] = element[1] * eigts3_local[0] + element[0] * eigts3_local[1];
+		for (is = 0 ; is < nspin_mag; is++) {
 
-			sup = becsum[ ijh + ( ina * first_becsum ) + ( is * first_becsum * nat ) ];
+			aux_sup[0] = aux[ ( ig + (is * ngm) ) * 2 ];
+			aux_sup[1] = aux[ ( ( ig + (is * ngm) ) * 2 ) + 1 ];
+
+			sup = becsum[ ijh + ( it * first_becsum ) + ( is * first_becsum * nat ) ];
 
 			skk[0] = skk[0] * sup;
 			skk[1] = skk[1] * sup;
@@ -74,14 +69,10 @@ __global__ void kernel_compute_aux( const double * __restrict eigts1, const doub
 
 			element[1] = skk[1] * s_qgm[ 0 ] + skk[0] * s_qgm[ 1 ];
 			aux_sup[1] = aux_sup[1] + element[1];
-
-			}
-
+	
+			aux[ ( ig + (is * ngm) ) * 2 ] = aux_sup[0];
+			aux[ ( ( ig + (is * ngm) ) * 2 ) + 1 ] = aux_sup[1];
 		}
-
-		aux[ ( ig + (is * ngm) ) * 2 ] = aux_sup[0];
-		aux[ ( ( ig + (is * ngm) ) * 2 ) + 1 ] = aux_sup[1];
-
 	}
 }
 
@@ -90,9 +81,9 @@ extern "C" int addusdens_cuda_(int * ptr_nr1, int * ptr_nr2, int * ptr_nr3, int 
 		double * ylmk0, double * eigts1, double * eigts2, double * eigts3, int * ig1, int * ig2,
 		int * ig3, double * aux, double * becsum, int * ityp, int * ptr_nspin_mag, int * ptr_nspin)
 {
-	int ijh, ih, jh, na, iih, jjh;
+	int ijh, ih, jh, na, iih, jjh, iit;
 
-	void * ityp_D, * qgm_D, * becsum_D, * aux_D;
+	void * qgm_D, * becsum_D, * aux_D;
     void * eigts1_D, * eigts2_D, * eigts3_D;
     void * ig1_D, * ig2_D, * ig3_D;
 
@@ -106,15 +97,15 @@ extern "C" int addusdens_cuda_(int * ptr_nr1, int * ptr_nr2, int * ptr_nr3, int 
 	int nspin = (* ptr_nspin);
 	int first_becsum = (* ptr_first_becsum );
 
-	dim3 threads2_aux(1, __CUDA_TxB_ADDUSDENS_COMPUTE_AUX__);
-	dim3 grid2_aux( qe_compute_num_blocks(nspin_mag, threads2_aux.x), qe_compute_num_blocks(ngm, threads2_aux.y) );
+	dim3 threads2_aux(__CUDA_TxB_ADDUSDENS_COMPUTE_AUX__);
+	dim3 grid2_aux( qe_compute_num_blocks(ngm, threads2_aux.x) );
 
 #if defined(__CUDA_DEBUG)
 	printf("\n[ADDUSDENS] Enter \n");fflush(stdout);
 #endif
 
-	if ( grid2_aux.y > __CUDA_MAXNUMBLOCKS__) {
-		fprintf( stderr, "\n[ADDUSDENS] kernel_compute_aux cannot run, blocks requested ( %d ) > blocks allowed!!!", grid2_aux.y );
+	if ( grid2_aux.x > __CUDA_MAXNUMBLOCKS__) {
+		fprintf( stderr, "\n[ADDUSDENS] kernel_compute_aux cannot run, blocks requested ( %d ) > blocks allowed!!!", grid2_aux.x );
 		return 1;
 	}
 
@@ -136,8 +127,6 @@ extern "C" int addusdens_cuda_(int * ptr_nr1, int * ptr_nr2, int * ptr_nr3, int 
 	shift += ( first_becsum * nat * nspin )*sizeof(double);
 	qgm_D = (char*) qe_dev_scratch[0] + shift;
 	shift += ( ngm * 2 )*sizeof(double);
-	ityp_D = (char*) qe_dev_scratch[0] + shift;
-	shift += ( (nat%2==0 ? nat : nat+1) )*sizeof( int );
 	eigts1_D = (char*) qe_dev_scratch[0] + shift;
 	shift += ( ( ( nr1 * 2 + 1 ) * nat ) * 2 )*sizeof(double);
 	eigts2_D = (char*) qe_dev_scratch[0] + shift;
@@ -167,15 +156,12 @@ extern "C" int addusdens_cuda_(int * ptr_nr1, int * ptr_nr2, int * ptr_nr3, int 
 
 	qecudaSafeCall( cudaMemcpy( (double *) aux_D, aux,  sizeof( double ) * ( ngm * nspin_mag * 2 ), cudaMemcpyHostToDevice ) );
 	qecudaSafeCall( cudaMemcpy( (double *) becsum_D, becsum,  sizeof( double ) * ( first_becsum * nat * nspin ), cudaMemcpyHostToDevice ) );
-	qecudaSafeCall( cudaMemcpy( (int * ) ityp_D, ityp,  sizeof( int ) * nat, cudaMemcpyHostToDevice ) );
 	qecudaSafeCall( cudaMemcpy( ig1_D, ig1,  sizeof( int ) * ngm, cudaMemcpyHostToDevice ) );
 	qecudaSafeCall( cudaMemcpy( ig2_D, ig2,  sizeof( int ) * ngm, cudaMemcpyHostToDevice ) );
 	qecudaSafeCall( cudaMemcpy( ig3_D, ig3,  sizeof( int ) * ngm, cudaMemcpyHostToDevice ) );
 	qecudaSafeCall( cudaMemcpy( eigts1_D, eigts1,  sizeof( double ) * ( ( ( nr1 * 2 + 1 ) * nat ) * 2 ), cudaMemcpyHostToDevice ) );
 	qecudaSafeCall( cudaMemcpy( eigts2_D, eigts2,  sizeof( double ) * ( ( ( nr2 * 2 + 1 ) * nat ) * 2 ), cudaMemcpyHostToDevice ) );
 	qecudaSafeCall( cudaMemcpy( eigts3_D, eigts3,  sizeof( double ) * ( ( ( nr3 * 2 + 1 ) * nat ) * 2 ), cudaMemcpyHostToDevice ) );
-
-	qecudaSafeCall( cudaFuncSetCacheConfig(kernel_compute_aux, cudaFuncCachePreferShared) );
 
 	ijh = 0;
 
@@ -188,20 +174,25 @@ extern "C" int addusdens_cuda_(int * ptr_nr1, int * ptr_nr2, int * ptr_nr3, int 
 
 			qecudaSafeCall( cudaMemcpy( (double *) qgm_D, qgm,  sizeof( double ) * ngm * 2, cudaMemcpyHostToDevice ) );
 
-			kernel_compute_aux<<< grid2_aux, threads2_aux >>>( (double *) eigts1_D,
-					(double *) eigts2_D, (double *) eigts3_D,
-					(int *)  ig1_D, (int *)  ig2_D,
-					(int *)  ig3_D, nr1, nr2, nr3, (double *) qgm_D,
-					(double *) becsum_D, (double *) aux_D, na, nspin_mag, ngm,
-					first_becsum, ijh, nat, (int *) ityp_D, nt );
-			qecudaGetLastError("kernel launch failure");
-//			cudaDeviceSynchronize();
+			for( iit = 0; iit < nat ; iit++ ) {
+
+				if ( ityp[iit] == nt ) {
+
+					kernel_compute_aux<<< grid2_aux, threads2_aux >>>(
+							(double *) eigts1_D, (double *) eigts2_D, (double *) eigts3_D,
+							(int *) ig1_D, (int *)  ig2_D, (int *) ig3_D, nr1, nr2, nr3,
+							(double *) qgm_D, (double *) becsum_D, (double *) aux_D,
+							na, nspin_mag, ngm, first_becsum, ijh, nat, iit );
+					qecudaGetLastError("kernel launch failure");
+
+				}
+
+			}
 
 		}
 	}
 
 	qecudaSafeCall( cudaMemcpy( aux, (double *) aux_D, sizeof( double ) * ( ngm * nspin_mag * 2 ), cudaMemcpyDeviceToHost ) );
-//	qecudaSafeCall( cudaFuncSetCacheConfig(kernel_compute_aux, cudaFuncCachePreferNone) );
 
 #if defined(__CUDA_NOALLOC)
 	/* Deallocating... */
