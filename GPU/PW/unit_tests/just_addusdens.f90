9e6563c642f220b873fdfa7bb21cd8be8c400a02
Levi Barnes <lbarnes@nvidia.com>
2013-07-23 14:23:54 +0000

    Unit tests for newd, addusdens and vloc_psi

---
+++ GPU/PW/unit_tests/just_addusdens.f90
@@ -0,0 +1,103 @@
+program main
+   USE kinds,                ONLY : DP
+   USE uspp,                 ONLY : nlx, lpl, lpx, ap, indv, nhtolm
+   USE uspp_param,           ONLY : nbetam, lmaxq, nhm
+   USE us,                   ONLY : dq, qrad
+
+   CHARACTER(LEN=32) :: filename  = "addusdens_input001.bin"
+   INTEGER :: nr1, nr2, nr3, first_becsum, nat, nt, ngm
+   INTEGER, ALLOCATABLE :: nh(:)
+   DOUBLE PRECISION, ALLOCATABLE :: qmod(:), ylmk0(:,:)
+   COMPLEX(DP), ALLOCATABLE :: qgm(:)
+   COMPLEX(DP), ALLOCATABLE :: eigts1(:)
+   COMPLEX(DP), ALLOCATABLE :: eigts2(:)
+   COMPLEX(DP), ALLOCATABLE :: eigts3(:)
+   INTEGER, ALLOCATABLE :: mill(:,:)
+   INTEGER :: ityp
+   COMPLEX(DP), ALLOCATABLE :: aux(:,:)
+   DOUBLE PRECISION, ALLOCATABLE :: becsum(:,:,:)
+   INTEGER :: nspin
+   INTEGER :: nspin_mag
+
+   INTEGER :: sizeA, sizeB, sizeC, sizeD
+
+   INTEGER, EXTERNAL :: addusdens_cuda
+   INTEGER :: err
+
+
+
+   CALL InitCudaEnv()
+   OPEN(UNIT= 11, STATUS = 'OLD', FILE = filename, FORM='UNFORMATTED') 
+    ALLOCATE(nh(10))
+    READ(11) nr1
+    READ(11) nr2
+    READ(11) nr3
+    READ(11) first_becsum
+    READ(11) nat
+    READ(11) nh
+    READ(11) nt
+    READ(11) ngm
+    READ(11) lmaxq
+    READ(11) nspin_mag
+    READ(11) nspin
+
+    ALLOCATE(aux(ngm,nspin_mag))
+    ALLOCATE(qmod(ngm))
+    ALLOCATE(qgm(ngm))
+    ALLOCATE(ylmk0(ngm,lmaxq*lmaxq))
+    ALLOCATE(eigts1((nr1*2+1)*nat))
+    ALLOCATE(eigts2((nr2*2+1)*nat))
+    ALLOCATE(eigts3((nr3*2+1)*nat))
+    ALLOCATE(mill(3,ngm))
+    ALLOCATE(becsum(first_becsum, nat, nspin))
+ 
+    READ(11) qmod
+    READ(11) qgm
+    READ(11) ylmk0
+    READ(11) eigts1
+    READ(11) eigts2
+    READ(11) eigts3
+    READ(11) mill
+    READ(11) aux
+    READ(11) becsum
+    READ(11) ityp
+ 
+    READ(11) sizeA
+    READ(11) sizeB
+    PRINT *, "ALLOCATE(indv(", sizeA, ",", sizeB, "))"
+    ALLOCATE(indv(sizeA, sizeB))
+    READ(11) indv
+   
+    READ(11) sizeA
+    READ(11) sizeB
+    PRINT *, "ALLOCATE(nhtolm(", sizeA, ",", sizeB, "))"
+    ALLOCATE(nhtolm(sizeA, sizeB))
+    READ(11) nhtolm
+   
+    READ(11) sizeA
+    READ(11) sizeB
+    READ(11) sizeC
+    READ(11) sizeD
+    PRINT *, "ALLOCATE(qrad(", sizeA, ",", sizeB, ",", sizeC, ",", sizeD, "))"
+    ALLOCATE(qrad(sizeA, sizeB, sizeC, sizeD))
+    READ(11) qrad
+
+    READ(11) lpx
+    READ(11) lpl
+    READ(11) ap
+  
+    READ(11) nbetam
+
+
+    CLOSE(11)
+
+
+
+
+   		err = addusdens_cuda(nr1, nr2, nr3, first_becsum, nat, nh, nt, ngm, qmod, qgm, ylmk0, &
+  		    eigts1, eigts2, eigts3, mill (1,:), mill (2,:), mill (3,:), &
+  		    aux, becsum, ityp, nspin_mag, nspin, qrad, size(qrad,1), size(qrad,2), size(qrad,3), &
+          size(qrad,4), lmaxq, nlx, dq, indv, nhtolm, nbetam, lpx, lpl, ap, size(ap,1), nhm)
+  		!
+   
+end program
---
+++ GPU/PW/unit_tests/just_vloc_psi.f90
@@ -0,0 +1,75 @@
+PROGRAM main
+  USE kinds,     ONLY : DP
+  IMPLICIT NONE
+
+  INTEGER :: lda, n, m, nnr, nr1x, nr2x, nr3x, ngms, ngm, npwx, ierr
+  COMPLEX(DP), ALLOCATABLE :: psi(:,:)
+  COMPLEX(DP), ALLOCATABLE :: hpsi(:,:)
+  REAL(DP), ALLOCATABLE :: v(:)
+
+  INTEGER, EXTERNAL :: vloc_psi_cuda_k
+#if defined(__CUDA_MULTIPLAN_FFT)
+  INTEGER, EXTERNAL :: vloc_psi_multiplan_cuda
+#else
+  INTEGER, EXTERNAL :: vloc_psi_cuda
+#endif
+
+  INTEGER, ALLOCATABLE :: nls(:), nlsm(:), igk(:)
+  CHARACTER(LEN=32) :: filename
+  LOGICAL :: file_exists
+
+  WRITE(filename, "(A16,I0.3,A4)"), "vloc_psi_k_input", 0, ".bin"
+  INQUIRE(FILE=filename, EXIST=file_exists)
+  IF (.not.file_exists) then
+    PRINT *, "Could not find input file $", filename, "$"
+    RETURN
+  ENDIF
+  
+  CALL InitCudaEnv()
+  PRINT *," Opening file ", filename
+  OPEN(UNIT=11, STATUS = 'OLD', FILE = filename, FORM='UNFORMATTED')
+  READ (11) lda
+  READ (11) nnr
+  READ (11) nr1x
+  READ (11) nr2x
+  READ (11) nr3x
+  READ (11) n
+  READ (11) m
+  ALLOCATE(psi(lda,m))
+  READ (11) psi
+
+  ALLOCATE(v(nnr))
+  READ (11) v
+
+  ALLOCATE(hpsi(lda,m))
+  READ (11) hpsi
+
+  READ (11) npwx
+  READ (11) ngms
+  READ (11) ngm
+  ALLOCATE(igk(npwx))
+  READ (11) igk
+
+  ALLOCATE(nls(ngms))
+  READ (11) nls
+
+  CLOSE (11)
+
+! __CUDA_MULTIPLAN_FFT broken?
+!#if defined(__CUDA_MULTIPLAN_FFT)
+!  CALL vloc_psi_multiplan_cuda_k ( lda, dffts%nnr, dffts%nr1x, dffts%nr2x, dffts%nr3x, &
+!     n, m, psi, vrs(1,current_spin), hpsi, igk(1:), nls(1:), ngms)
+!#else
+  ierr = vloc_psi_cuda_k ( lda, nnr, nr1x, nr2x, nr3x, &
+    n, m, psi, v, hpsi, igk(1:), nls(1:), ngms)
+!#endif
+! Below if for gamma
+!#if defined(__CUDA_MULTIPLAN_FFT)
+!  ierr =   vloc_psi_multiplan_cuda ( lda, nnr, nr1x, nr2x, nr3x, &
+!    n, m, psi, v, hpsi, igk(1:), nls(1:), nlsm(1:), ngms, ngm)
+!#else
+!  ierr = vloc_psi_cuda ( lda, nnr, nr1x, nr2x, nr3x, &
+!     n, m, psi, v, hpsi, igk(1:), nls(1:), nlsm(1:), ngms, ngm)
+!#endif
+
+END program
